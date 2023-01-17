4e99f99d552762a50bc5695cf0fc61fa58eb134a
Juliya Sartasova <anjee2007@mail.ru>
2023-01-17 14:51:44 +0300

    fix goods title height

--- src/modules/mainPage/mainPage.js
+++ src/modules/mainPage/mainPage.js
@@ -2,8 +2,7 @@ import {renderNavigation} from "../render/renderNavigation.js";
 import {renderHero} from "../render/renderHero.js";
 import {renderProducts} from "../render/renderProducts.js";
 
-export const mainPage = (gender) => {
-    console.log(gender);
+export const mainPage = (gender = 'women') => {
     renderNavigation();
     renderHero();
     renderProducts();

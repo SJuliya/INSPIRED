d19e77295ddfa1f36be455a0b21d2002777fa69c
Juliya Sartasova <anjee2007@mail.ru>
2023-03-31 15:04:37 +0300

    change API URL

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,6 +1,6 @@
 export const COUNT_PAGINATION = 3;
 export const DATA = {};
-export const API_URL = 'http://localhost:8024';
+export const API_URL = 'https://maple-neighborly-calf.glitch.me';
 export const TITLE = {
     women: {
         title: 'Новая коллекция Бюстгальтер-балконет',
